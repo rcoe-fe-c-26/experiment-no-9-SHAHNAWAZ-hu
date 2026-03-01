47104ca37db6bfe1115091d15ad60b422b74979e
SAYYED SHAHNAWAZ <hassangoussayyed@eng.rizvi.edu.in>
2026-03-01 17:10:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
-
+# Coder: sayyed shahnawaz
+# Date: 20/02/26
 print("--- Factorial Finder ---\n")
 
 
-# Write your code here
+# Write your code here:
+a = int(input("Enter Number: "))
+if a<0:
+    print(f"Factorial of {a} is Not Defined")
+fact = 1
+for x in range(1,a+1):
+    fact = fact*x 
+print(f"Factorial of {a} is",fact)
+
